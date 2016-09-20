46e4e1871249485dac285ff97362c841f62b4cdb
DavidStyrbjorn <davidstyrbjorn@gmail.com>
2016-09-20 20:44:42 +0200

    Added basic Paddle functionality

--- Game.py
+++ Game.py
@@ -1,25 +1,37 @@
 import pygame
+from Paddle import Paddle
 
 class Game:
     def __init__(self):
         pygame.init()
-            
+
+        # Screen dimensions used for various calculations
         self.width = 800
         self.height = 600
 
-        self.isRunning = True
+        self.isRunning = True # initial value should be True at start
         
-        self.screen = pygame.display.set_mode((self.width,self.height))
-        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((self.width,self.height)) # The game screen
+        self.clock = pygame.time.Clock() # Clock for limiting frames per second
+        
+        self.all_sprites_list = pygame.sprite.Group() # sprite list contating all the game sprites
+        self.player_one = Paddle(20, 5) # Creating an object called player_one
+        self.player_two = Paddle(self.width-20 - 15, 5)
+        self.all_sprites_list.add(self.player_one, self.player_two) # Adding player_one to our sprite list
         
     def run(self):
         while self.isRunning:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                self.player_one.getInput(event, pygame.K_w, pygame.K_s)
+                self.player_two.getInput(event, pygame.K_UP, pygame.K_DOWN)
+                if event.type == pygame.QUIT: # If the user clicked QUIT then exit out
                     self.isRunning = False
 
-            self.screen.fill((255,255,255))
+            self.screen.fill((255,255,255)) # Filling the screen with a white color
 
+            # Update and draw all sprites
+            self.all_sprites_list.update()
+            self.all_sprites_list.draw(self.screen)
 
             self.clock.tick(60)            
             pygame.display.update()
--- Paddle.py
+++ Paddle.py
@@ -2,21 +2,26 @@ import pygame
 
 class Paddle(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        pygame.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface((15,25))
+        self.image = pygame.Surface((15,80))
         self.rect = self.image.get_rect()
 
         self.rect.x = x
-        self.rect.y = y
+        self.rect.y = y 
 
-        self.change_x = 0
-        self.change_y = =
-
-    def getInput(self, event, up_key, down_key, right_key, left_key):
-
-    def move(self):
-        
+        self.change_y = 0
+        self.velocity = 9 # pixels per frame
 
     def update(self):
-        
+        self.rect.y += self.change_y * self.velocity
+
+    def getInput(self, event, up_key, down_key):
+        if(event.type == pygame.KEYDOWN):
+            if(event.key == up_key):
+                self.change_y = -1
+            if(event.key == down_key):
+                self.change_y = 1
+        elif (event.type == pygame.KEYUP):
+            if(event.key == up_key or event.key == down_key):
+                self.change_y = 0
